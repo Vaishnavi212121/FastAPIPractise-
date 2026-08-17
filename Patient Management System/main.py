8dea69b66931901287e6d409e3b628b1850662a7
Vaishnavi Patil <vaishnavipatil9018@gmail.com>
2026-08-17 12:50:25 +0530

    Intialize

---
+++ Patient Management System/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return{'message':'Patient Management System'}
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+
+    return data
+
+@app.get('/about')
+def about():
+    return{'message':'A fully functional API to manage patient record'}
+
+@app.get('/view')
+def view():
+    data =  load_data()
+
+    return data
